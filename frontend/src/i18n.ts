dca6f956354ebbe62eb3d4b9251b671a56183ad2
AmrTamer23 <amrtamer23mj@gmail.com>
2024-10-22 22:39:15 +0300

    chore(frontend): presist lang & placeholder logo

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,23 +1,29 @@
 import { initReactI18next } from "react-i18next"
 import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 
 import arTranslation from "@/locales/ar/translation.json"
 import enTranslation from "@/locales/en/translation.json"
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: enTranslation,
+i18n
+  .use(LanguageDetector) // Add the language detector
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      ar: {
+        translation: arTranslation,
+      },
     },
-    ar: {
-      translation: arTranslation,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
     },
-  },
-  lng: "en",
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-})
+    detection: {
+      lookupLocalStorage: "lang",
+    },
+  })
 
 export default i18n
